de5292c998782a99d8f648068625b7ac47a8ee99
emb <emb@catfact.net>
2016-10-26 20:44:01 -0400

    don't define debug print functions for release build

--- asf/avr32/utils/debug/print_funcs.h
+++ asf/avr32/utils/debug/print_funcs.h
@@ -4,7 +4,7 @@
  *
  * \brief Strings and integers print module for debug purposes.
  *
- * Copyright (c) 2009-2015 Atmel Corporation. All rights reserved.
+ * Copyright (c) 2009-2012 Atmel Corporation. All rights reserved.
  *
  * \asf_license_start
  *
@@ -41,9 +41,6 @@
  * \asf_license_stop
  *
  ******************************************************************************/
-/*
- * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
- */
 
 
 #ifndef _PRINT_FUNCS_H_
@@ -60,7 +57,8 @@
 
 #include <avr32/io.h>
 #include "board.h"
-
+#include "types.h"
+#include "conf_board.h"
 
 /*! \name USART Settings for the Debug Module
  */
@@ -307,47 +305,8 @@ extern void init_dbg_rs232(long pba_hz);
  */
 extern void init_dbg_rs232_ex(unsigned long baudrate, long pba_hz);
 
-/*! \brief Prints a string of characters to DBG_USART.
- *
- * \param str The string of characters to print.
- */
-extern void print_dbg(const char *str);
 
-/*! \brief Prints a character to DBG_USART.
- *
- * \param c The character to print.
- */
-extern void print_dbg_char(int c);
 
-/*! \brief Prints an integer to DBG_USART in a decimal representation.
- *
- * \param n The integer to print.
- */
-extern void print_dbg_ulong(unsigned long n);
-
-/*! \brief Prints a char to DBG_USART in an hexadecimal representation.
- *
- * \param n The char to print.
- */
-extern void print_dbg_char_hex(unsigned char n);
-
-/*! \brief Prints a short integer to DBG_USART in an hexadecimal representation.
- *
- * \param n The short integer to print.
- */
-extern void print_dbg_short_hex(unsigned short n);
-
-/*! \brief Prints an integer to DBG_USART in an hexadecimal representation.
- *
- * \param n The integer to print.
- */
-extern void print_dbg_hex(unsigned long n);
-
-/*! \brief Prints a string of characters to a given USART.
- *
- * \param usart Base address of the USART instance to print to.
- * \param str The string of characters to print.
- */
 extern void print(volatile avr32_usart_t *usart, const char *str);
 
 /*! \brief Prints a character to a given USART.
@@ -389,5 +348,73 @@ extern void print_hex(volatile avr32_usart_t *usart, unsigned long n);
 /**
  * \}
  */
+
+
+
+#if RELEASEBUILD==1
+
+#define  print_dbg(blahblah) ;;
+#define  print_dbg_char(blahblah) ;;
+#define  print_dbg_ulong(blahblah) ;;
+#define  print_dbg_char_hex(blahblah) ;;
+#define  print_dbg_short_hex(blahblah) ;;
+#define  print_dbg_hex(blahblah) ;;
+
+#else
+
+/*! \brief Prints a string of characters to DBG_USART.
+ *
+ * \param str The string of characters to print.
+ */
+extern void print_dbg(const char *str);
+
+/*! \brief Prints a character to DBG_USART.
+ *
+ * \param c The character to print.
+ */
+extern void print_dbg_char(int c);
+
+/*! \brief Prints an integer to DBG_USART in a decimal representation.
+ *
+ * \param n The integer to print.
+ */
+extern void print_dbg_ulong(unsigned long n);
+
+/*! \brief Prints a char to DBG_USART in an hexadecimal representation.
+ *
+ * \param n The char to print.
+ */
+extern void print_dbg_char_hex(unsigned char n);
+
+/*! \brief Prints a short integer to DBG_USART in an hexadecimal representation.
+ *
+ * \param n The short integer to print.
+ */
+extern void print_dbg_short_hex(unsigned short n);
+
+/*! \brief Prints an integer to DBG_USART in an hexadecimal representation.
+ *
+ * \param n The integer to print.
+ */
+extern void print_dbg_hex(unsigned long n);
+
+/*! \brief Prints a string of characters to a given USART.
+ *
+ * \param usart Base address of the USART instance to print to.
+ * \param str The string of characters to print.
+ */
+
+
+// print an array of bytes
+extern void print_byte_array (u8* data, u32 size, u32 linebreak);
+
+// print a unicode string (stripping to ascii)
+void print_unicode_string(char* str, u32 len);
+
+
+#endif // release build
+
+
+
 
 #endif  // _PRINT_FUNCS_H_

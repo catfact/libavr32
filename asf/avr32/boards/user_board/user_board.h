e9abc826cb6ada6d804a4d2df8db69c199239065
emb <emb@catfact.net>
2016-10-28 11:10:47 -0400

    Revert "include conf_board.h from user_board.h"
    This reverts commit 4abd3e46483aa1ab17d004d075bf4b6e2ff6fbc0.

--- asf/avr32/boards/user_board/user_board.h
+++ asf/avr32/boards/user_board/user_board.h
@@ -11,8 +11,6 @@
 #ifndef USER_BOARD_H
 #define USER_BOARD_H
 
-#include "conf_board.h"
-
 /* This file is intended to contain definitions and configuration details for
  * features and devices that are available on the board, e.g., frequency and
  * startup time for an external crystal, external memory devices, LED and USART
